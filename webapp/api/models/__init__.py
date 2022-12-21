235def26e761e9792ffc644636e12611bc9f0d52
Alex Shulman <ajshulman@gmail.com>
2022-12-21 15:27:32 -0700

    Alex Shulman: Model for location (untested)

--- webapp/api/models/__init__.py
+++ webapp/api/models/__init__.py
@@ -2,3 +2,4 @@ from .base import Base  # NOQA
 # import all your model classes here
 from .equipment import Equipment
 from .event import Event
+from .location import Location
---
+++ webapp/api/models/location.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, DateTime, Integer, String
+
+from .base import Base
+
+class Location(Base):
+    """Model for provided data/locations.csv"""
+    
+    __table__name = "location"
+
+    id = Column(Integer, primary_key=True)
+    city = Column(String)
+    city_long = Column(String)
+    station = Column(String)
+    fsac = Column(String) # could be a int w/ more context
+    scac = Column(String)
+    splc = Column(String) # could be a int w/ more context
+    state = Column(String)
+    time_zone = Column(String)
+    # If geospatial computations are important
+    # lat/lng should be PostGIS geog items
+    # SRID=4326;POINT(longitude latitude)'
+    longitude = Column(String)
+    latitude = Column(String)
+    country = Column(String)
