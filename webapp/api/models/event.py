f778aa3588308064827b051c10952af3fcb18d45
Alex Shulman <ajshulman@gmail.com>
2022-12-21 15:27:02 -0700

    Alex Shulman: Model for event (untested)

---
+++ webapp/api/models/event.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, DateTime, Integer, String
+
+from .base import Base
+
+class Event(Base):
+    """Model for provided data/events.csv"""
+    
+    __table__name = "event"
+
+    id = Column(Integer, primary_key=True)
+    equipment_id = Column(String)
+    sighting_date = Column(DateTime)
+    sighting_event_code = Column(Integer)
+    reporting_railroad_scac = Column(String)
+    posting_date = Column(DateTime)
+    from_mark_id = Column(String)
+    load_empty_status = Column(String)
+    sighting_claim_code  = Column(String)
+    sighting_event_code_text  = Column(String)
+    train_id  = Column(String)
+    train_alpha_code  = Column(String)
+    location_id = Column(Integer)
+    waybill_id = Column(Integer)
+    customer = Column(String)
+    fleet = Column(String)
+    
