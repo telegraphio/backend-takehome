aa445d59a5412088cbe93a948011f512ade5c9cf
Alex Shulman <ajshulman@gmail.com>
2022-12-22 14:28:14 -0700

    Alex Shulman: Add posting_date param to /events and /waybills/<id>/events

--- webapp/api/controllers/events.py
+++ webapp/api/controllers/events.py
@@ -12,18 +12,17 @@ class GetEvents:
 
     def on_get(self, req: Request, resp: Response):
 
-        posting_date_start = req.get_param('posting_date_start') or None
-        posting_date_end = req.get_param('posting_date_end') or None
         posting_date = req.get_param('posting_date') or None
 
         if posting_date:
             # Parse Posting Date
             try:
                 parsed_datetime = datetime.strptime(posting_date, "%Y-%m-%d %H:%M:%S")
-                resp.media = [ x.toDict() for x in self.session.query(Event).filter(posting_date==parsed_datetime).all() ]
-
+                events = [ x.toDict() for x in self.session.query(Event).filter(
+                    Event.posting_date==parsed_datetime
+                    ).all()]
+                resp.media = events
             except ValueError as e:
                 resp.media = {"Error Parsing Posting Date": posting_date}
-            # return filtered by posting date data
         else:
             resp.media = [ x.toDict() for x in self.session.query(Event).all()]
--- webapp/api/controllers/waybills.py
+++ webapp/api/controllers/waybills.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from falcon import Request, Response
 
 # from api.db import SQLAlchemy
@@ -33,11 +35,24 @@ class GetEquipmentByWaybillID:
 
 class GetEventsByWaybillID:
 
-    def on_get(self, _: Request, resp: Response, waybill_id):
-        # We should check that a waybill exists before filtering the Events table
+    def on_get(self, req: Request, resp: Response, waybill_id):
+        posting_date = req.get_param('posting_date') or None
         waybill = self.session.query(Waybill).get(waybill_id)
-        events = self.session.query(Event).filter(Event.waybill_id==waybill.id).all()
-        resp.media = [x.toDict() for x in events]
+        if posting_date:
+            # Parse Posting Date
+            try:
+                parsed_datetime = datetime.strptime(posting_date, "%Y-%m-%d %H:%M:%S")
+                events = [ x.toDict() for x in self.session.query(Event).filter(
+                    Event.posting_date==parsed_datetime
+                    and 
+                    Event.waybill_id==waybill.id
+                    ).all()]
+                resp.media = events
+            except ValueError as e:
+                resp.media = {"Error Parsing Posting Date": posting_date}
+        else:
+            events = self.session.query(Event).filter(Event.waybill_id==waybill.id).all()
+            resp.media = [x.toDict() for x in events]
 
 class GetLocationsByWaybillID:
 
